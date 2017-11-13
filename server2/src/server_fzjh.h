78b0211ca8f2a279246368f60e21251f961e5c09
zhuziyu1157817544 <1157817544@qq.com>
2017-11-13 21:35:27 +0800

    fzjh

--- server2/src/server_fzjh.h
+++ server2/src/server_fzjh.h
@@ -7,8 +7,22 @@
 
 #ifndef _SERVER_FZJH_H
 #define _SERVER_FZJH_H
+typedef struct
+{
+    int weight;
+    int now_weight;
+    ip_home *next;
+    int att ;//当前服务器是否可用 ,0不可用1可用
+}ip_home;
+
 class fzjh
 {
+public:
+    ip_home *home_use;//当前服役的服务器
+    ip_home *home_now;//当前该选它作为负载均衡的服务器
+    ip_home *homw_lazy;//目前gg服务器
+    int ip_number;
+    int over;//是否轮寻完一圈,1轮寻完一圈，要重置了,0没有轮寻完一圈
 
 };
 #endif
