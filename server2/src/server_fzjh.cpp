a0e23ccf68caf52b3e7cfe3668eee15136b74876
zhuziyu1157817544 <1157817544@qq.com>
2017-11-15 20:45:47 +0800

    fzjh

--- server2/src/server_fzjh.cpp
+++ server2/src/server_fzjh.cpp
@@ -6,6 +6,69 @@
  ************************************************************************/
 
 #include<iostream>
+#include<string>
 #include"server_fzjh.h"
 using namespace std;
+using std::string;
+
+
+fzjhs::fzjh()//负载均衡类的构造函数
+{
+    //...
+}
+bool fzjh::addip(string ip,int weight)//添加监控ip
+{
+    //...
+}
+bool fzjh::delip(string ip)//删除无效ip
+{
+    //...
+}
+bool fzjh::reset()//重置ip队列
+{
+    //...
+}
+(ip_homes) fzjh::get_ip()//获取此次应该选择的ip_home
+{
+    
+}
+(ip_homes) fzjh::get_ok_ips()//获取当前可用的ip队列
+{
+    //...
+}
+(ip_homes) fzjh::get_bad_ips()//获取当前不可用ip的队列
+{
+    //...
+}
+int fzjh::get_ip(string ip)//获取对应ip的属性值
+{
+    //...
+}
+
+int fzjh::get_ip_number()//获取总ip数量
+{
+    //...
+}
+
+int fzjh::get_ok_ip_number()//获取可用ip数量
+{
+    //.,,
+}
+
+int fzjh::get_bad_ip_number()//获取不可用ip数量
+{
+    //...
+}
+
+bool fzjh::ip_weight_hash()
+//进行ip到weight的哈希
+{
+    //...
+}
+
+bool fzjh::set_ip_attr(string ip)//改变ip的属性
+{
+    //...
+}
+
 
--- server2/src/server_fzjh.h
+++ server2/src/server_fzjh.h
@@ -7,41 +7,46 @@
 
 #ifndef _SERVER_FZJH_H
 #define _SERVER_FZJH_H
-typedef struct
+#include <string>
+#include <iostream>
+#include <ptypes.h>
+using namespace std;
+class ip_homes
 {
+public:
     string ip;//存放ip
     int weight;//也算是性质标志
     int now_weight;
     ip_home *next;
-    int att ;//当前服务器是否可用 ,0不可用1可用
+    bool att ;//当前服务器是否可用 ,0不可用1可用
     void * endpoint;//用作结尾标识符,为以后的hash作
-}ip_home;
+};
 
 class fzjh
 {
 private:
-    ip_home *home_use;//当前服役的服务器头头
-    ip_home *home_use_end;//当前服役的服务器尾巴
-    ip_home *home_now;//当前该选它作为负载均衡的服务器
-    ip_home *homw_lazy;//目前gg服务器
-    ip_home **buckets;//为以后ip hash用
+    ip_homes *home_use;//当前服役的服务器头头
+    ip_homes *home_use_end;//当前服役的服务器尾巴
+    ip_homes *home_now;//当前该选它作为负载均衡的服务器
+    ip_homes *homw_lazy;//目前gg服务器
+    ip_homes **buckets;//为以后ip hash用
     int ip_number;//ip数量
     int ip_ok_number;
     int ip_bad_number;
     int over;//是否轮寻完一圈,1轮寻完一圈，要重置了,0没有轮寻完一圈
 public:
     fzjh();//构造函数
-    int addip(string &ip,int &weight);//添加新的ip
-    int delip(string &ip,int &weight);//删除旧的ip
-    int reset();//重置ip队列
-    (ip_home*) & get_ip();//获取负载均衡的ip  
-    int get_ok_ips();//列出当前的可用ip和属性们
-    int get_bad_ips();//列出当前不可用的ip和属性们
-    void search_ip(string ip);//获取所需要的ip属性
+    bool addip(string ip,int weight);//添加新的ip
+    bool delip(string ip);//删除旧的ip
+    bool reset();//重置ip队列
+    (ip_homes) get_ip();//获取负载均衡的ip  
+    (ip_homes) get_ok_ips();//列出当前的可用ip和属性们
+    (ip_homes) get_bad_ips();//列出当前不可用的ip和属性们
+    int get_ip(string ip);//获取所需要的ip属性
     int get_ok_ip_number();//获取可用ip数量
     int get_ip_number();//获取总ip数量
     int get_bad_ip_number();//获取不可用ip数量
-    int ip_weight_hash();//进行ip到weight的哈希
-    int set_ip_attr();//本质上用hash比较好
+    bool ip_weight_hash();//进行ip到weight的哈希
+    bool set_ip_attr(string ip);//本质上用hash比较好
 };
 #endif
